695ac5800e40804d8a535b74fffd1643e5eaf4c2
Meng87 <mroupurplax@gmail.com>
2022-12-03 20:41:56 -0500

    Add side panel

---
+++ my-app/src/components/SidePanel/SidePanel.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './SidePanel.css'
+
+export default function SidePanel(props) {
+  return (
+      <div className="side-panel">
+        I am a Side Panel
+      </div>
+  )
+}
