c2aaa3d1e41902057cf71b9803fff3e2eaf156fa
Meng87 <mroupurplax@gmail.com>
2022-12-28 02:08:12 -0500

    Optimize triple to double loop when modifying grid

--- my-app/src/components/GridBoard/GridBoard.js
+++ my-app/src/components/GridBoard/GridBoard.js
@@ -9,10 +9,10 @@ export default function GridBoard(props) {
     // Generate sample change array
     const coords = []
     let c = 0
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < 250; i++) {
         coords.push([])
         for (let j = 0; j < 32; j++) {
-            coords[i].push([0+c,j])
+            coords[i].push([(0 + c) % 32, j])
         }
         c += 1
     }
@@ -38,20 +38,20 @@ export default function GridBoard(props) {
             return
         }
         let turn = coords[index]
+        // Create a copy of the initial grid
         const nextGrid = grid.map((row, i) => {
             return(
                 row.map((elem, j) => {
-                    for (let change of turn) {
-                        let x = change[0]
-                        let y = change[1]
-                        if (i === y && j === x) {
-                            return <GridSquare key={`${x}${y}`} color="1" />
-                        }
-                    }
-                    return elem
+                    return <GridSquare key={`${j}${i}`} color="0" />
                 })
             )
         })
+        // Modify grid based on changes
+        for (let change of turn) {
+            let x = change[0]
+            let y = change[1]
+            nextGrid[y][x] = <GridSquare key={`${x}${y}`} color="1" />
+        }
         setGrid(nextGrid)
     }, [index])
 
@@ -62,7 +62,7 @@ export default function GridBoard(props) {
     let runAnimation = () => {
         console.log("Running animation...")
         if (!intervalID) {
-            const intID = setInterval(iterateFrames, 1000)
+            const intID = setInterval(iterateFrames, 50)
             setIntervalID(intID)
         }
     }
