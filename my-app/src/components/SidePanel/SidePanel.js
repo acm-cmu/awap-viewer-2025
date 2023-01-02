bc94dabb7fee02192000f9bcd39a9398f810e264
Meng87 <mroupurplax@gmail.com>
2023-01-01 23:27:21 -0500

    Fix formatting

--- my-app/src/components/SidePanel/SidePanel.js
+++ my-app/src/components/SidePanel/SidePanel.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react'
-import './SidePanel.css'
-import Button from 'react-bootstrap/Button'
+import React, { useState } from "react"
+import "./SidePanel.css"
+import Button from "react-bootstrap/Button"
 
 export default function SidePanel(props) {
   const [framePlaying, setframePlaying] = useState(false)
@@ -20,27 +20,33 @@ export default function SidePanel(props) {
   const showFile = async (event) => {
     // object.preventDefault()
     const reader = new FileReader()
-    reader.onload = async (e) => { 
+    reader.onload = async (e) => {
       const replay_text = e.target.result
       try {
-          var replay_object = JSON.parse(props.replayData)
-      }
-      catch(err) {
-          console.log(err.message)
+        var replay_object = JSON.parse(props.replayData)
+      } catch (err) {
+        console.log(err.message)
       }
       props.onFileData(replay_object)
       alert(replay_text)
-    };
+    }
     reader.readAsText(event.target.files[0])
   }
 
   return (
-      <div className="side-panel">
-        <h1>
-          <font face="Impact" size="5">AWAP 2023 Viewer</font><br />
-        </h1>
-        <input type="file" onChange={showFile} /><br /><br />
-        <Button id="play-button" onClick={changePlay}>Play</Button>
-      </div>
+    <div className="side-panel">
+      <h1>
+        <font face="Impact" size="5">
+          AWAP 2023 Viewer
+        </font>
+        <br />
+      </h1>
+      <input type="file" onChange={showFile} />
+      <br />
+      <br />
+      <Button id="play-button" onClick={changePlay}>
+        Play
+      </Button>
+    </div>
   )
 }
