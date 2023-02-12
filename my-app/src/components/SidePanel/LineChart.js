69aa977af42ceae588b53456e9f13ebbfc31c670
Meng87 <mroupurplax@gmail.com>
2023-02-12 12:47:24 -0500

    Fix style

--- my-app/src/components/SidePanel/LineChart.js
+++ my-app/src/components/SidePanel/LineChart.js
@@ -1,11 +1,7 @@
-import React, {
-  useContext,
-} from "react"
+import React, { useContext } from "react"
 
-import Chart from 'react-google-charts'
-import { ViewerContext } from '../../pages/Viewer'
-
-
+import Chart from "react-google-charts"
+import { ViewerContext } from "../../pages/Viewer"
 
 /*
   [0, 0, 0],
@@ -17,8 +13,7 @@ import { ViewerContext } from '../../pages/Viewer'
   [6, 11, 3],
   [7, 27, 19],
   [8, 27, 200],
-*/ 
-
+*/
 
 const LineChartOptions = {
   //title: "Metal Reserves",
@@ -26,102 +21,83 @@ const LineChartOptions = {
   //title: 'Company Performance',
   //subtitle: 'Sales, Expenses, and Profit: 2014-2017',
 
-  'chartArea': {
-      'backgroundColor': {
-          'fill': '#F4F4F4',
-          'opacity': 100,
-      },
+  chartArea: {
+    backgroundColor: {
+      fill: "#F4F4F4",
+      opacity: 100,
+    },
   },
 
-  titleTextStyle:{
-      fontName: "Impact",
-      fontSize: 20,
+  titleTextStyle: {
+    fontName: "Impact",
+    fontSize: 20,
   },
   hAxis: {
-      title: 'Frame',
-      titleTextStyle:
-      {
-       fontName: "Roboto",
-       fontStyle: "bold", //or bold, italic, etc.
-       italic: false,
-       fontSize: 12,
-       //color: '#ff0000'
-      },
+    title: "Frame",
+    titleTextStyle: {
+      fontName: "Roboto",
+      fontStyle: "bold", //or bold, italic, etc.
+      italic: false,
+      fontSize: 12,
+      //color: '#ff0000'
+    },
   },
-vAxis: {
-  title: 'Metal',
-  titleTextStyle:
-  {
-   fontName: "Roboto",
-   fontStyle: "bold", //or bold, italic, etc.
-   italic: false,
-   fontSize: 12,
-   //color: '#ff0000'
+  vAxis: {
+    title: "Metal",
+    titleTextStyle: {
+      fontName: "Roboto",
+      fontStyle: "bold", //or bold, italic, etc.
+      italic: false,
+      fontSize: 12,
+      //color: '#ff0000'
+    },
+    viewWindow: {
+      min: 0,
+    },
   },
-  viewWindow: {
-    min: 0
-  }
-},
-series: {
-  1: { curveType: 'function' },
-},
+  series: {
+    1: { curveType: "function" },
+  },
 }
 
+function LineChart() {
+  const { frame, setFrame, redMetal, blueMetal, setRedMetal, setBlueMetal } =
+    useContext(ViewerContext)
 
-function LineChart (){
-    const {
-      frame,
-      setFrame,
-      redMetal,
-      blueMetal,
-      setRedMetal,
-      setBlueMetal
-    } = useContext(ViewerContext)
+  var i = 0
 
+  const LineData = [
+    ["Frame", "Blue Team", "Red Team"],
+    [0, 0, 0],
+  ]
 
-    var i = 0
-    //console.log("frame " + frame)
-
-    const LineData = [
-      ['Frame', 'Blue Team', 'Red Team'],
-      [0, 0, 0]
-    ]
-
-
-    //LineData.push(['Frame', 'Blue Team', 'Red Team'])
-    //LineData.push([0,0,0])
-    for(let redval of redMetal){
-      if (i == frame) {
-        break;
-      }
-      //console.log(i)
-      const temp = []
-      temp.push(i)
-      temp.push(blueMetal[i])
-      temp.push(redMetal[i])
-      LineData.push(temp)
-      i+=1
+  //LineData.push(['Frame', 'Blue Team', 'Red Team'])
+  //LineData.push([0,0,0])
+  for (let redval of redMetal) {
+    if (i == frame) {
+      break
     }
+    const temp = []
+    temp.push(i)
+    temp.push(blueMetal[i])
+    temp.push(redMetal[i])
+    LineData.push(temp)
+    i += 1
+  }
 
-    console.log("LineChart: " + LineData)
-
-    return (
-      <div className="container mt-5">
-        <h1>Metal Graph</h1>
-        <Chart
-          width={'350px'}
-          height={'250px'}
-          chartType="LineChart"
-          loader={<div>Loading Chart</div>}
-          data={LineData}
-          options={LineChartOptions}
-          rootProps={{ 'data-testid': '3' }}
-        />
-      </div>
-    )
-  
+  return (
+    <div className="container mt-5">
+      <h1>Metal Graph</h1>
+      <Chart
+        width={"350px"}
+        height={"250px"}
+        chartType="LineChart"
+        loader={<div>Loading Chart</div>}
+        data={LineData}
+        options={LineChartOptions}
+        rootProps={{ "data-testid": "3" }}
+      />
+    </div>
+  )
 }
 export default LineChart
-
-//<h1>{redMetal}</h1>
-        
