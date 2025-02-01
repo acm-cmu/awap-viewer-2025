dca0a473f3ba24e3596cce8e16a731ebca68f8c6
Jennifer Zhu <jzhucs128@gmail.com>
2025-01-31 19:45:47 -0500

    changed healthbar type to tsx

--- src/components/GridBoard/HealthBar.tsx
+++ src/components/GridBoard/HealthBar.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
+  
+  export type HealthBarProps = {
+    health: number
+  }
 
-
-const HealthBar = (props) => {
+const HealthBar = (props: HealthBarProps) => {
     const { health } = props
     return <div className='health-bar'>
         <progress value={health} max={10}>  </progress>
