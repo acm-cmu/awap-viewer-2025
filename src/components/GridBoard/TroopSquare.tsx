e08acfdc42d319dd7797c1b25e25881b06ba7fda
Jennifer Zhu <jzhucs128@gmail.com>
2025-01-31 19:51:28 -0500

    changed troopsquare type to tsx

---
+++ src/components/GridBoard/TroopSquare.tsx
@@ -0,0 +1,37 @@
+import React from "react"
+import "./Grid.css"
+import "./Troop.css"
+import HealthBar from "./HealthBar"
+
+/*
+  Type labels:
+  0 - red
+  1 - blue
+*/
+
+export enum TroopSquareType {
+  Red = 0,
+  Blue = 1
+}
+
+export type TroopSquareProps = {
+  color: TroopSquareType,
+  type: string,
+  lvl: number,
+  health: number,
+  attack_range: number, 
+  damage: number, 
+  defense: number, 
+  damage_range: number
+}
+
+export default function TroopSquare(props: TroopSquareProps) {
+  const { color, type, lvl, health, attack_range, damage, defense, damage_range } = props
+  return (
+    <div className={`tile-div`}>
+      {<HealthBar health={health} />}
+      {<div className={`circle ${color}-${type}`}></div>}
+    </div>
+  )
+}
+
