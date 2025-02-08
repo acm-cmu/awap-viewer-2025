04b42a42975b1f5ac2b0b53202311e3817ae8fc6
Jennifer Zhu <jzhucs128@gmail.com>
2025-02-07 23:47:53 -0500

    fixed viewer page tsx completely

--- src/pages/Viewer.tsx
+++ src/pages/Viewer.tsx
@@ -1,7 +1,7 @@
 import './MainPage.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import React, { Dispatch, SetStateAction, createContext, Provider, useState } from 'react';
+import React, { ReactNode, Dispatch, SetStateAction, createContext, Provider, useState } from 'react';
 
 import B1 from '../assets/MapTiles/TileB1.png';
 import B2 from '../assets/MapTiles/TileB2.png';
@@ -14,7 +14,7 @@ import N3 from '../assets/MapTiles/TileN3.png';
 import N4 from '../assets/MapTiles/TileN4.png';
 import N5 from '../assets/MapTiles/TileN5.png';
 import GridBoard from '../components/GridBoard/GridBoard.js';
-import SidePanel from '../components/SidePanel/SidePanel.js';
+import SidePanel, { Replay } from '../components/SidePanel/SidePanel.js';
 
 /*
   Color labels:
@@ -26,87 +26,83 @@ import SidePanel from '../components/SidePanel/SidePanel.js';
   #0 blue
 */
 
+type ColorKeyType = {
+  GRASS: string
+}
+
+type StatsType = number[]
+
+export type TogglePageType = {
+  togglePage: () => void
+}
+
 export interface ViewerContextTypes{
-  replay: object,
+  redTroops: ReactNode[],
+  setRedTroops: Dispatch<SetStateAction<ReactNode[]>>,
+  blueTroops: ReactNode[],
+  setBlueTroops: Dispatch<SetStateAction<ReactNode[]>>,
+  redCastles: ReactNode[],
+  setRedCastles: Dispatch<SetStateAction<ReactNode[]>>,
+  blueCastles: ReactNode[],
+  setBlueCastles: Dispatch<SetStateAction<ReactNode[]>>,
+  redStats: StatsType,
+  setRedStats: Dispatch<SetStateAction<StatsType>>,
+  blueStats: StatsType,
+  setBlueStats: Dispatch<SetStateAction<StatsType>>,
+  replay: Replay | null,
+  setReplay: Dispatch<SetStateAction<Replay | null>>,
   sliderValue: number,
   setSliderValue: Dispatch<SetStateAction<number>>,
   isPlay: boolean,
   setIsPlay: Dispatch<SetStateAction<boolean>>,
-  framePlaying: Dispatch<SetStateAction<boolean>>,
-  setIsFinished: Dispatch<SetStateAction<boolean>>,
-  speed: number,
-  setMetadata:  Dispatch<SetStateAction<(string | number)[]>>,
-  setTimeout:  (boolean | null)[],
-  colorKey: number,
-  RandTileColor: number,
+  framePlaying: boolean,
+  setFramePlaying: Dispatch<SetStateAction<boolean>>,
+  timeout: (boolean | null)[],
+  setTimeout:  Dispatch<SetStateAction<(boolean | null)[]>>,
+  colorKey: ColorKeyType,
+  RandTileColor: (color: string) => number | null,
   normalImgArray: HTMLImageElement[],
   blockedImgArray: HTMLImageElement[]
 };
 
-const ViewerContext = createContext<ViewerContextTypes>({});
+const ViewerContext = createContext<ViewerContextTypes | undefined>(undefined);
 
-function Viewer({ togglePage} ) {
+function Viewer({ togglePage}: TogglePageType ) {
   // for randomization of tile choice
-  const colorKey = { GRASS: '0' };
+  const colorKey: ColorKeyType = { GRASS: '0' };
   const blockedImgCnt = 5;
   const normalImgCnt = 5;
   const blockedImgArray = [B1, B2, B3, B4, B5];
   const normalImgArray = [N1, N2, N3, N4, N5];
 
-  const [replay, setReplay] = useState(null);
-  const [metaData, setMetadata] = useState([0, 'blue']);
+  const [replay, setReplay] = useState<Replay | null>(null);
   const [timeout, setTimeout] = useState([false, null]);
 
   const [sliderValue, setSliderValue] = useState(1);
   const [isPlay, setIsPlay] = useState(false);
   const [framePlaying, setFramePlaying] = useState(false);
-  const [isDisabled, setIsDisabled] = useState(true);
-  const [isFinished, setIsFinished] = useState(false);
-  const [speed, setSpeed] = useState(1);
-  const [row, setRow] = useState(null);
-  const [col, setCol] = useState(null);
-  const [tiles, setTiles] = useState(null);
 
-  const [frame, setFrame] = useState(null);
 
-  const [redTroops, setRedTroops] = useState([]);
-  const [blueTroops, setBlueTroops] = useState([]);
+  const [redTroops, setRedTroops] = useState<ReactNode[]>([]);
+  const [blueTroops, setBlueTroops] = useState<ReactNode[]>([]);
 
-  const [redCastles, setRedCastles] = useState([]);
-  const [blueCastles, setBlueCastles] = useState([]);
+  const [redCastles, setRedCastles] = useState<ReactNode[]>([]);
+  const [blueCastles, setBlueCastles] = useState<ReactNode[]>([]);
 
   // [coinAmount, maxCastleHealth, currCastleHealth, farmlands]
   const [redStats, setRedStats] = useState([0, 0, 0, 0]);
   const [blueStats, setBlueStats] = useState([0, 0, 0, 0]);
 
-  const [isP1VisToggled, setIsP1VisToggled] = useState(false);
-  const [isP2VisToggled, setIsP2VisToggled] = useState(false);
-  const [isTrailToggled, setIsTrailToggled] = useState(false);
 
-  const handleFileData = (replayData) => {
+  const handleFileData = (replayData: Replay) => {
     setReplay(replayData);
     setSliderValue(1);
-    setIsDisabled(false);
-    const root = document.documentElement;
-    try {
-      root.style.setProperty('--cols', replayData[0].map.width);
-      root.style.setProperty('--rows', replayData[0].map.height);
-    } catch (err) {
-      console.log(err.message);
-    }
   };
 
-  const handleP1VisToggled = (toggleStatus) => {
-    setIsP1VisToggled(toggleStatus);
-  };
-
-  const handleP2VisToggled = (toggleStatus) => {
-    setIsP2VisToggled(toggleStatus);
-  };
 
   // for tile choices
 
-  function randomInt(min, max) {
+  function randomInt(min : number, max : number) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
@@ -118,7 +114,7 @@ function Viewer({ togglePage} ) {
     return randomInt(0, normalImgCnt - 1);
   }
 
-  function RandTileColor(color) {
+  function RandTileColor(color: string) {
     switch (Number(color)) {
       case 0:
         return RandNormalTile();
@@ -142,8 +138,6 @@ function Viewer({ togglePage} ) {
         setRedCastles,
         blueCastles,
         setBlueCastles,
-        frame,
-        setFrame,
         replay,
         setReplay,
         sliderValue,
@@ -152,22 +146,6 @@ function Viewer({ togglePage} ) {
         setIsPlay,
         framePlaying,
         setFramePlaying,
-        isDisabled,
-        setIsDisabled,
-        isFinished,
-        setIsFinished,
-        speed,
-        setSpeed,
-        row,
-        setRow,
-        col,
-        setCol,
-        tiles,
-        setTiles,
-        isTrailToggled,
-        setIsTrailToggled,
-        metaData,
-        setMetadata,
         timeout,
         setTimeout,
         colorKey,
